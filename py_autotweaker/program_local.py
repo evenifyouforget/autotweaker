8c73f059f3c281ada2b5006bb1f498e295760594
evenifyouforget <idiotvtuber@gmail.com>
2025-08-11 21:55:39 -0400

    make local runner better

--- py_autotweaker/program_local.py
+++ py_autotweaker/program_local.py
@@ -48,24 +48,36 @@ def program_local(args):
         # Set up garden
         garden = Garden([Creature(design_struct)], max_garden_size, config_data)
         
+        # Set up silenceable printing
+        silence_loop_print = False
+        def loop_print(*args, **kwargs):
+            if silence_loop_print:
+                return
+            print(*args, **kwargs)
         # Loop until end condition reached
-        for loop_count in itertools.count():
-            time_elapsed = time.time() - start_time
-            print(f'# Loop #{loop_count + 1} begins, total time {time_elapsed:.2f} seconds since start')
-            if time_elapsed > timeout_seconds:
-                print(f'Stopping due to time limit reached ({timeout_seconds} seconds)')
-                break
-            garden_status = garden.checkup()
-            print(f'Best score so far: {garden_status.best_score}')
-            if stop_on_win and garden_status.best_score <= 0:
-                print('Stopping early since a solve was found')
-                break
-            garden.evolve(max_threads, upload_best_k)
-            garden.start(max_threads - garden_status.num_active_threads)
-            print(f'Statistics: {garden.num_kills} creatures have been culled so far')
+        try:
+            for loop_count in itertools.count():
+                # Don't print every loop
+                silence_loop_print = loop_count > 100 and loop_count % 10**(len(str(loop_count)) - 2)
+                # Do the loop
+                time_elapsed = time.time() - start_time
+                loop_print(f'# Loop #{loop_count + 1} begins, total time {time_elapsed:.2f} seconds since start, using {max_threads} threads and {max_garden_size} creatures')
+                if time_elapsed > timeout_seconds:
+                    loop_print(f'Stopping due to time limit reached ({timeout_seconds} seconds)')
+                    break
+                garden_status = garden.checkup()
+                loop_print(f'Best score so far: {garden_status.best_score}')
+                if stop_on_win and garden_status.best_score <= 0:
+                    loop_print('Stopping early since a solve was found')
+                    break
+                garden.evolve(max_threads, upload_best_k)
+                garden.start(max_threads - garden_status.num_active_threads)
+                loop_print(f'Statistics: {garden.num_kills} creatures have been culled so far')
+        except KeyboardInterrupt:
+            print('Stopping early due to user interrupt')
 
         # Save results
         print(f'Uploading up to {upload_best_k} best creatures')
         for rank, creature in enumerate(garden.creatures[:upload_best_k]):
-            saved_design_id = save_design(creature.design_struct, user_id)
+            saved_design_id = save_design(creature.design_struct, user_id, name=f'Auto {rank+1}', description=f'Based on {args.design_id}, score {creature.best_score}')
             print(f'{rank+1}. Saved to https://ft.jtai.dev/?designId={saved_design_id} achieving score of {creature.best_score}')
